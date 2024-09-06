8c7cb274c0fd37e18d797a6dd70afec49bfeb62a
Erik Öberg <eriobe@miun.se>
2024-09-06 14:41:00 +0200

    lagt till klassen Ariplane

---
+++ ConflictProjektet/Airplane.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConflictProjektet
+{
+    public class Airplane
+    {
+        public int Altitude { get; }
+        public string TypeName => "Flygplan";
+
+        public double Speed => 1;
+        public double Fuel => 1;
+        public void Fly()
+        {
+
+        }
+    }
+}
