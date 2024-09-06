013e956c61bf20cfcb1e87fe7260db013ea8ac79
Erik Öberg <eriobe@miun.se>
2024-09-06 14:28:27 +0200

    Add project files.

---
+++ ConflictProjektet/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ConflictProjektet
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
